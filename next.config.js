88c61015f13dcbf9737749f3c9a53cc1a55f0548
xiaoshuai22334455 <158548406+xiaoshuai22334455@users.noreply.github.com>
2024-06-26 12:11:27 +0800

    Update next.config.js
    update config

--- next.config.js
+++ next.config.js
@@ -28,5 +28,11 @@ module.exports = withBundleAnalyzer(
     experimental: {
       serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
     }
+   // 添加环境变量
+      env: {
+          METADATA_BASE: 'https://gpt4.aliyu.vip', // 更换为你的应用的实际URL
+          NEXT_PUBLIC_SUPABASE_URL: 'https://snphcdgpsamvpgzmztep.supabase.co', // 更换为你的Supabase项目URL
+          NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNucGhjZGdwc2FtdnBnem16dGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2NDA2NDksImV4cCI6MjAyNTIxNjY0OX0.7YrTo7r03DIjK64cAnGukNW-jikMt9SuF0zoqB6TgTI', // 更换为你的Supabase匿名密钥
+      },
   })
 )
